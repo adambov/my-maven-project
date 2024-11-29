474398c5f16343821a8f6c7740d854684bfba495
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2024-11-29 15:19:39 +0200

    adding close,quit,thread.sleep

--- src/test/java/practice/Practice1.java
+++ src/test/java/practice/Practice1.java
@@ -7,9 +7,17 @@ import org.testng.annotations.Test;
 public class Practice1 {
 
     @Test
-    public void verifyIcanStartSeleniumOnMachine () {
+    public void verifyIcanStartSeleniumOnMachine () throws java. lang. InterruptedException{
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Atanas\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         driver.get("https://www.google.bg/");
+
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        String url = driver.getCurrentUrl();
+
+        Thread.sleep(4444);
+        driver.close();
+        driver.quit();
     }
 }

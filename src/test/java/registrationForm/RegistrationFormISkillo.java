8dc10009b46f9ad3f3f45da049b306b9f7f0260a
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2024-11-30 18:43:45 +0200

    adding registration form happy path automation

--- src/test/java/registrationForm/RegistrationFormISkillo.java
+++ src/test/java/registrationForm/RegistrationFormISkillo.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -30,8 +31,36 @@ public class RegistrationFormISkillo {
         WebElement logInAnchor = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@id, 'nav-link-login')]")));
         logInAnchor.click();
 
+        WebElement notAMember = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='color-black']")));
+        String actualNotAMember = notAMember.getText();
+        Assert.assertEquals(actualNotAMember,"Not a member?");
+
         WebElement registerAnchor = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Register']")));
         registerAnchor.click();
+
+        WebElement usernameRegFormInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Username']")));
+        WebElement emailRegFormInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='email']")));
+        WebElement birthDateRegFormInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Birth date']")));
+        WebElement passwordRegFormInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Password']")));
+        WebElement confirmPasswordRegFormInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Confirm Password']")));
+        WebElement publicInfoRegForm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@placeholder='Public info']")));
+        WebElement signInButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='sign-in-button']")));
+
+        boolean isDisabled = signInButton.getAttribute("disabled") != null;
+
+        Assert.assertTrue(isDisabled);
+
+        usernameRegFormInput.sendKeys("NaskoDambov");
+        emailRegFormInput.sendKeys("atanas.dambov@gmail.com");
+        birthDateRegFormInput.sendKeys("1997-12-16");
+        passwordRegFormInput.sendKeys("Password123");
+        confirmPasswordRegFormInput.sendKeys("Password123");
+        publicInfoRegForm.sendKeys("Hello, It's me :)");
+
+        boolean isEnabled = signInButton.getAttribute("disabled") == null;
+        Assert.assertTrue(isEnabled);
+
+        signInButton.click();
 
         Thread.sleep(5555);
         driver.quit();

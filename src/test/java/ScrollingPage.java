dc5e6b262571db322793d2487587843d5ea006a5
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2024-12-02 18:53:52 +0200

    reparing scroll

--- src/test/java/ScrollingPage.java
+++ src/test/java/ScrollingPage.java
@@ -2,14 +2,12 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 import java.time.Duration;
-import java.util.List;
+
 
 public class ScrollingPage {
     WebDriver driver = new ChromeDriver();
@@ -25,24 +23,12 @@ public class ScrollingPage {
     private void scrollingToPic() throws java. lang. InterruptedException{
 
         Actions actions = new Actions(driver);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-
 
         for (int i = 0; i < 10; i++) {
             actions.sendKeys(Keys.PAGE_DOWN).perform();
             Thread.sleep(500);
-
-            List<WebElement> images = driver.findElements(By.tagName("img"));
-
-            for (WebElement image : images) {
-                if ((image.isDisplayed())) {
-                    Thread.sleep(500);
-                    image.click();
-                    System.out.println("Кликнато изображение: " + image.getAttribute("src"));
-                    break;
-                }
-            }
         }
+
 
     }
 }
